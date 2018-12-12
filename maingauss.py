53b56667e6053dbcbe489c6b0ff8fc0f722832e3
Laurens Samson <laurenssamson@hotmail.com>
2018-12-12 13:31:30 +0100

    Fixed

--- maingauss.py
+++ maingauss.py
@@ -7,52 +7,60 @@ import torch
 import torch.nn.functional as F
 helper = Helper()
 
-param = 'exp'
-episodes = 1000
-NUMBER_OF_RUNS = 20
-alpha_van = 0.5
-alpha_nat = alpha_van
+
+param = 'sigma'
+episodes = 10000
+
+NUMBER_OF_RUNS = 1
+
+
+if param == 'exp':
+	alpha_van = 0.05
+	alpha_nat = 0.2
+
+if param == "sigma":
+	alpha_van = 0.05
+	alpha_nat = 0.05
 smooth = 10
 
-# alpha voor exp was 0.2 voor allebei
+# alpha voor exp was 0.2 en 0.05 voor allebei
 
-def sigma(sigma):
+def sigma_param(sigma):
 	if param == 'exp':
 		return np.exp(sigma)
-	if param == 'prec':
-		return 1/sigma**2
 	if param == 'sigma':
-		return np.abs(sigma)
+		return sigma**2
 
 def derivative_sigma_param(sigma, mean, action, fisher_mean_sum, fisher_sigma_sum, fisher_N, fisher=False):
 	if param == 'exp':
-		mean_grad = (action - mean ) / np.exp(sigma)**2
-		sigma_grad = -1 + (action - mean)**2 / np.exp(sigma)**2
+		mean_grad = (action - mean) / np.exp(sigma)**2
+		sigma_grad = -1 + ((action - mean)**2 / np.exp(sigma)**2)
 		if fisher:
 			fisher_N += 1
-			fisher_mean_sum += 1/np.exp(sigma)**2 # 1/sigma(theta)
+			fisher_mean_sum += 1/np.exp(sigma)**2 
 			fisher_mean_mean = fisher_mean_sum / fisher_N
 			fisher_mean_inverse = 1/fisher_mean_mean
-			
 			fisher_sigma_sum = fisher_sigma_sum
 			fisher_sigma_inverse = 1/2
 			return mean_grad * fisher_mean_inverse, sigma_grad * fisher_sigma_inverse,  fisher_mean_sum, fisher_sigma_sum, fisher_N
 		return mean_grad, sigma_grad
-	if param == 'prec':
-		mean_grad = (action - mean ) / (1/sigma**2)**2
-		sigma_grad = 2/sigma - sigma**3 * (action - mean)**2
-		if fisher:
-			return mean_grad * (1/sigma**2)**2, sigma_grad * sigma**2/3
-		return mean_grad, sigma_grad
 	if param == 'sigma':
-		mean_grad = (action - mean ) / sigma**2
-		sigma_grad = -1/sigma + (action - mean)**2 / sigma**3
+		mean_grad = (action - mean)/(sigma**4)
+		sigma_grad = (-2/sigma) + ((action - mean)**2 * 2 * sigma)/(sigma_param(sigma)**3)
 		if fisher:
-			return mean_grad * sigma**2 , 0.5 * sigma**2 * sigma_grad
+			fisher_N += 1
+			fisher_mean_sum += 1/sigma_param(sigma)**2
+			fisher_mean_mean = fisher_mean_sum / fisher_N
+			fisher_mean_inverse = 1/fisher_mean_mean
+
+			fisher_sigma_sum +=  8/sigma_param(sigma)
+			fisher_sigma_mean = fisher_sigma_sum/fisher_N
+			fisher_sigma_inverse = 1/fisher_sigma_mean
+			return mean_grad * fisher_mean_inverse, sigma_grad * fisher_sigma_inverse, fisher_mean_sum, fisher_sigma_sum, fisher_N
 		return mean_grad, sigma_grad
 
 
-def rewardfunction(action):
+def rewardfunction(distribution):
 	normal = torch.distributions.Normal(4, 2)
 	reward = np.exp(normal.log_prob(action)).numpy()
 	return reward
@@ -63,26 +71,30 @@ NG_results = np.zeros((NUMBER_OF_RUNS, episodes))
 
 for t in range(NUMBER_OF_RUNS):
 	model = Gauss() 
+	print(model.mean, model.std, "INITALISIATION")
 	returns= []
 	losses = []
 	fisher_mean_sum = 0
 	fisher_sigma_sum = 0
 	fisher_N = 0 
 	for i in range(episodes):
-		action, log_action = model.forward(sigma)
+		action, log_action = model.forward(sigma_param)
 		G = rewardfunction(action)
 		returns.append(G)
-		if model.std < 1e-1:
-			model.std = 1e-1
-		grad_mean, grad_sigma = derivative_sigma_param(model.std, model.mean, action, fisher_mean_sum, fisher_sigma_sum, fisher_N, fisher=False)
 
-		model.std = model.std + alpha_van * grad_sigma * G
+		grad_mean, grad_sigma = derivative_sigma_param(model.std, model.mean, action, fisher_mean_sum, fisher_sigma_sum, fisher_N, fisher=False)
+		
+
+		model.std =  np.clip(model.std + alpha_van * grad_sigma * G, 1, -1)
+
 		model.mean = model.mean + alpha_van * grad_mean * G
 
-		loss = - G * log_action
+		loss = -G * log_action
 		losses.append(loss)
-	# print(model.mean, model.std)
+	print(model.mean, model.std)
+
 	SGD_results[t] = returns
+
 
 	returns_NG = []
 	model = Gauss()
@@ -90,17 +102,13 @@ for t in range(NUMBER_OF_RUNS):
 	fisher_sigma_sum = 0
 	fisher_N = 0 
 	for i in range(episodes):
-		action, log_action = model.forward(sigma)
+		action, log_action = model.forward(sigma_param)
 		G = rewardfunction(action)
 		returns_NG.append(G)
-		if model.std < 1e-1:
-			model.std = 1e-1
 		grad_mean, grad_sigma, fisher_mean_sum, fisher_sigma_sum, fisher_N = derivative_sigma_param(model.std, model.mean, action, fisher_mean_sum, fisher_sigma_sum, fisher_N, fisher=True)
 
-
-		model.std = model.std + alpha_nat * grad_sigma * G
+		model.std = np.clip(model.std + alpha_nat * grad_sigma * G, 1, -1)
 		model.mean = model.mean + alpha_nat * grad_mean * G
-
 		loss = - G * log_action
 
 	NG_results[t] = returns_NG
@@ -112,8 +120,8 @@ var_return_SGD = helper.smooth(np.std(SGD_results, axis=0), smooth)
 average_return_NG = helper.smooth(np.mean(NG_results, axis=0), smooth)
 var_return_NG = helper.smooth(np.std(NG_results, axis=0), smooth)
 
-print(average_return_SGD[len(average_return_SGD)-1])
-print(average_return_NG[len(average_return_NG)-1])
+print(np.mean(average_return_SGD[-10:-1]), "SGD")
+print(np.mean(average_return_NG[-10:-1]), "NG")
 
 plt.plot(average_return_NG, color="red")
 plt.fill_between(np.arange(len(average_return_NG)), average_return_NG - var_return_NG, average_return_NG + var_return_NG, color="red", alpha=0.2)
